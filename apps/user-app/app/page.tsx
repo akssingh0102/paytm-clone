92430fb477da627237ab7f1f07c182683ad0a044
akssingh0102 <akashsingh095@gmail.com>
2024-07-21 00:29:43 +0530

    WIP: Setup project

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,6 +1,12 @@
-import { PrismaClient } from '@repo/db/client';
-import { Test } from '@repo/ui/test';
+"use client"
+import { signIn, signOut, useSession } from "next-auth/react";
+import { Appbar } from "@repo/ui/appbar";
 
-export default function Home() {
-  return <Test />;
+export default function Page(): JSX.Element {
+  const session = useSession();
+  return (
+   <div>
+      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user} />
+   </div>
+  );
 }
